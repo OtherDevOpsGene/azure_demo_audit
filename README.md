76fa63e14a227189a8e33ce5570d00c5786e5d74
Ubuntu <ggotimer@ggworkstation1.qvmw5uohqvyeli0kwqycxegvhe.bx.internal.cloudapp.net>
2019-06-11 17:41:39 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# demo_audit
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: demo_audit
+# Recipe:: default
+#
+# Copyright:: 2019, The Authors, All Rights Reserved.
